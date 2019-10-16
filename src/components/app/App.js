87d4c0cbe87ffadea900102925337d6b85f1664c
azizmashkour <azizmashkour@gmail.com>
2019-10-16 16:31:46 +0100

    feat: implement translation feature

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import Select from 'react-select';
+import { withTranslation } from 'react-i18next';
+import { options } from '../../configs/options';
 import './App.css';
 
-const appStyles = {
-  fontSize: '7rem',
-  textAligne: 'center',
-}
+const App = ({lang, i18n, t}) => {
+  const appStyles = {
+    fontSize: '7rem',
+    textAligne: 'center',
+  }
 
-const App = () => (
-  <div style={appStyles}>Welcome to <span>RighCare</span></div>
-);
+  const changeLang = (lang) => {
+    const { value } = lang;
+    i18n.changeLanguage(value);
+  };
 
-export default App;
+
+  return (
+    <div style={appStyles}>
+      {t('Welcome to')} <span>RighCare</span>
+      <div className="App__language">
+        <Select
+          defaultValue={options[0]}
+          options={options}
+          value={lang}
+          onChange={changeLang}
+          className="App-Select"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default withTranslation()(App);
