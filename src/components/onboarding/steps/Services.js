6fcc3e91d5e960f312020c21dd1a9fecd76f1de9
azizmashkour <azizmashkour@gmail.com>
2019-11-11 16:33:48 +0100

    feat: add dynamic fake channels to services

--- src/components/onboarding/steps/Services.js
+++ src/components/onboarding/steps/Services.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import MailIcon from '../../../assets/images/onboard/services/add-message.svg';
-import AnswerIcon from '../../../assets/images/onboard/services/answer.svg';
-import PhoneIcon from '../../../assets/images/onboard/services/call-back.svg';
-import WebIcon from '../../../assets/images/onboard/services/id-card.svg';
-import FaceIcon from '../../../assets/images/onboard/services/facebook.svg';
-import TwitIcon from '../../../assets/images/onboard/services/twitter.svg';
+import FakeChannels from '../../../faker/channels';
 
 const Services = (props) => {
   const {
-    t,
     kind,
+    checkedItems,
+    handleChange,
   } = props;
 
   const serviceStyle = {
@@ -23,40 +19,38 @@ const Services = (props) => {
       width: kind === 'channel' ? '21rem' : '28rem',
       margin: kind === 'channel' ? 'auto' : 'initial',
     },
+    input: {
+      display: 'none',
+    },
   };
 
   return (
     <div className="service-container" style={serviceStyle.serviceContainer}>
-      <div className="service-card">
-        <img src={MailIcon} alt="mail service" style={serviceStyle.firstImage} />
-        <span>Email</span>
-      </div>
-      <div className="service-card">
-        <img src={AnswerIcon} alt="answer service" />
-        <span>Live Chat</span>
-      </div>
-      <div className="service-card">
-        <img src={PhoneIcon} alt="phone service" />
-        <span>Live Chat</span>
-      </div>
-      <div className="service-card">
-        <img src={WebIcon} alt="web service" />
-        <span>Web form</span>
-      </div>
-      <div className="service-card">
-        <img src={FaceIcon} alt="facebook service" />
-        <span>Facebook</span>
-      </div>
-      <div className="service-card">
-        <img src={TwitIcon} alt="twitter service" />
-        <span>Twitter</span>
-      </div>
+      { FakeChannels.map((item, i) => (
+        <label
+          className={`${checkedItems[item.type] ? 'service-card is-selected-case' : 'service-card'}`}
+          control={item.type}
+          key={i}
+        >
+          <img src={item.greenIcon} alt={i} />
+          <span>{item.name}</span>
+          <input
+            type="checkbox"
+            name={item.type}
+            checked={checkedItems[item.type]}
+            onChange={handleChange}
+            style={serviceStyle.input}
+          />
+        </label>
+      ))}
     </div>
   );
 };
 
 Services.propTypes = {
-  t: PropTypes.func.isRequired,
+  kind: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  checkedItems: PropTypes.shape({}).isRequired,
 };
 
 export default withTranslation()(Services);
