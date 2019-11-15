99dd79901370b34b15f01b083476caabf67c9e56
azizmashkour <azizmashkour@gmail.com>
2019-11-15 10:38:21 +0100

    feat: add missing props

--- src/components/onboarding/steps/AddAgent.js
+++ src/components/onboarding/steps/AddAgent.js
@@ -10,7 +10,7 @@ const AddAgent = (props) => {
   } = props;
 
   return (
-    <Services handleChooseService={handleChooseService} checkedServices={checkedServices} />
+    <Services handleChooseService={handleChooseService} checkedServices={checkedServices} kind="agent" />
   );
 };
 
