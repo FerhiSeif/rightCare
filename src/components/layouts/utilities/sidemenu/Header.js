b4887f4d8ec0e31fb828a7d55a3024fad4fb4f29
azizmashkour <azizmashkour@gmail.com>
2019-11-15 16:31:28 +0100

    fix: fix url issues

--- src/components/layouts/utilities/sidemenu/Header.js
+++ src/components/layouts/utilities/sidemenu/Header.js
@@ -3,7 +3,7 @@ import DesktopLogo from '../../../../assets/images/logo/medium.png';
 
 const Header = () => (
   <div className="left-menu-logo-container">
-    <a className="navbar-item" href="!#">
+    <a className="navbar-item" href="/">
       <img alt="logo icon" src={DesktopLogo} />
     </a>
   </div>
