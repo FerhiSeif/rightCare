68d32a6802cf4d720e7fcd77b8ea443e61d90b35
azizmashkour <azizmashkour@gmail.com>
2019-11-18 11:20:51 +0100

    feat: create separated channel card header component

---
+++ src/components/common/shared/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+  const {
+    kind,
+    icon,
+    darkIcon,
+    initialAgents,
+    title,
+  } = props;
+
+  return (
+    <>
+      <header className="card-header">
+        <p className={`${kind === 'channel' ? 'card-header-title' : 'card-header-title agents'}`}>
+          <span className="icon">
+            <img src={`${kind === 'channel' ? icon : darkIcon}`} alt="Channel Icon" />
+          </span>
+          {title}
+        </p>
+        <a href="/onboard" className="card-header-icon" aria-label="more options">
+          { initialAgents && initialAgents.length > 0 ? (
+            <span className="icon">
+              {initialAgents.length}
+            </span>
+          ) : (
+            <span className="icon">
+              0
+            </span>
+          )}
+        </a>
+      </header>
+    </>
+  );
+};
+
+Header.propTypes = {
+  kind: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  darkIcon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  initialAgents: PropTypes.shape({}).isRequired,
+};
+
+export default Header;
