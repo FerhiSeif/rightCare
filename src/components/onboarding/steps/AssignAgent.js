598217aef5b0c8afe8f9bf2498edd3482615c084
azizmashkour <azizmashkour@gmail.com>
2019-11-18 11:19:55 +0100

    feat: fetch locally save agents if existed

--- src/components/onboarding/steps/AssignAgent.js
+++ src/components/onboarding/steps/AssignAgent.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import Carder from '../../common/Carder';
 import FakeChannels from '../../../faker/channels';
-import DarkEmailIcon from '../../../assets/images/onboard/channels/dark/add-message.svg';
 
 const AssignAgent = (props) => {
   const {
@@ -20,44 +19,59 @@ const AssignAgent = (props) => {
     localStorage.setItem('cr_services', JSON.stringify(selectedServices));
   }
 
+  const localService = JSON.parse(localStorage.getItem('cr_services'));
+
   return (
     <div className="card-container">
-      <Carder
-        kind="agent"
-        icon="not-found-icon"
-        darkIcon={DarkEmailIcon}
-        t={t}
-        title="Email"
-        content="No agent have been added"
-        buttonText="Add agent"
-        serviceCount={6}
-        agentAssigned
-        hasAgents
-        isChannelEmpty={false}
-        channelSelected={false}
-        handleChooseService={handleChooseService}
-        checkedServices={checkedServices}
-      />
-      { FakeChannels.filter((channel) => activeServices.indexOf(channel.type) >= 0)
-        .map((item, i) => (
-          <Carder
-            kind="agent"
-            icon={item.icon}
-            darkIcon={item.darkIcon}
-            key={i}
-            t={t}
-            title={item.type}
-            content="No agent have been added"
-            buttonText="Add agent"
-            hasAgents={false}
-            isChannelEmpty={false}
-            channelSelected={false}
-            serviceCount={6}
-            agentAssigned={false}
-            handleChooseService={handleChooseService}
-            checkedServices={checkedServices}
-          />
-        ))}
+      {
+        localService && localService.length > 0 ? (
+          <>
+            { localService.map((item, i) => (
+              <Carder
+                kind="agent"
+                icon={item.icon}
+                darkIcon={item.darkIcon}
+                key={i}
+                t={t}
+                title={item.type}
+                content="No agent have been added"
+                buttonText="Add agent"
+                isChannelEmpty={false}
+                channelSelected={false}
+                serviceCount={6}
+                agentAssigned={false}
+                handleChooseService={handleChooseService}
+                checkedServices={checkedServices}
+                assignedAgents={item.agents}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            { FakeChannels.filter((channel) => activeServices.indexOf(channel.type) >= 0)
+              .map((item, i) => (
+                <Carder
+                  kind="agent"
+                  icon={item.icon}
+                  darkIcon={item.darkIcon}
+                  key={i}
+                  t={t}
+                  title={item.type}
+                  content="No agent have been added"
+                  buttonText="Add agent"
+                  isChannelEmpty={false}
+                  channelSelected={false}
+                  serviceCount={6}
+                  agentAssigned={false}
+                  handleChooseService={handleChooseService}
+                  checkedServices={checkedServices}
+                  assignedAgents={item.agents}
+                />
+              ))}
+          </>
+        )
+      }
+
     </div>
   );
 };
