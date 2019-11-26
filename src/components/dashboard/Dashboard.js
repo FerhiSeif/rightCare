f7756cc571c0de3cfb6bbdd0779143d70cacec9e
azizmashkour <azizmashkour@gmail.com>
2019-11-26 11:19:48 +0100

    feat: get current widows size and pass to child component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../layouts/Header';
 import SideMenu from '../layouts/SideMenu';
 import { options } from '../../configs/options';
 
-const Dashboard = (props) => {
-  const {
-    t,
-    kind,
-    defaultLang,
-    changeLang,
-    isLogged,
-  } = props;
 
-  return (
-    <div className={`${kind === 'dashboard' ? 'columns' : ''}`}>
-      { kind === 'dashboard' && (<SideMenu t={t} />)}
-      <Header
-        options={options}
-        defaultLang={defaultLang}
-        changeLang={changeLang}
-        kind={kind}
-        isLogged={isLogged}
-        t={t}
-      />
-    </div>
-  );
-};
+class Dashboard extends Component {
+    state = {
+      containerWidth: 0,
+      containerHeight: 0
+    }
+
+    componentDidMount() {
+      this.updateWindowDimensions();
+      window.addEventListener('resize', this.updateWindowDimensions);
+      document.body.style.overflow = 'hidden';
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowDimensions);
+      document.body.style.overflow = 'auto';
+    }
+
+    updateWindowDimensions = () => {
+      this.setState({ containerWidth: window.innerWidth, containerHeight: window.innerHeight });
+    }
+
+    render() {
+      const {
+        t,
+        kind,
+        defaultLang,
+        changeLang,
+        isLogged,
+      } = this.props;
+      const { containerHeight, containerWidth } = this.state;
+
+      return (
+        <div className={`${kind === 'dashboard' ? 'columns' : ''}`}>
+          { kind === 'dashboard' && (<SideMenu t={t} />)}
+          <Header
+            options={options}
+            defaultLang={defaultLang}
+            changeLang={changeLang}
+            kind={kind}
+            isLogged={isLogged}
+            t={t}
+            containerWidth={containerWidth}
+            containerHeight={containerHeight}
+          />
+        </div>
+      );
+    }
+}
 
 Dashboard.propTypes = {
   t: PropTypes.func.isRequired,
