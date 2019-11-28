9b62154051bd89b9a33717bdec62084c7598ee9f
azizmashkour <azizmashkour@gmail.com>
2019-11-28 19:08:17 +0100

    fix: add message when agents not found from search

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -10,6 +10,7 @@ const Modal = (props) => {
     kind,
     buttonText,
     agentModal,
+    agentCount,
     handleChooseService,
     checkedServices,
     handleCloseRessourceModal,
@@ -30,6 +31,7 @@ const Modal = (props) => {
     <div className="modal" ref={agentModal}>
       <div className="modal-background" />
       <div className="modal-card">
+        <button className="modal-close modal-close-incard" aria-label="close" onClick={handleContinue} />
         <header className="modal-card-head">
           <div className="title-container" style={modalStyle.title}>
             <h2 className="title">{title}</h2>
@@ -42,7 +44,7 @@ const Modal = (props) => {
           )}
         </header>
         <section className="modal-card-body">
-          { kind === 'agent' ? (<div>{content}</div>) : <Services kind={kind} handleChooseService={handleChooseService} checkedServices={checkedServices} /> }
+          { kind === 'agent' ? (<div>{agentCount === 0 &&<span>This agent can not be found in the list.</span>}{content}</div>) : <Services kind={kind} handleChooseService={handleChooseService} checkedServices={checkedServices} /> }
         </section>
         <footer className="modal-card-foot">
           <button className="button is-primary" aria-label="close" onClick={handleContinue}>{buttonText}</button>
@@ -55,6 +57,7 @@ const Modal = (props) => {
 
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
+  agentCount: PropTypes.number.isRequired,
   content: PropTypes.shape({}).isRequired,
   kind: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
