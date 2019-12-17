5a1d3ce7ce0a9a81554943fd8d7b4288b472e895
azizmashkour <azizmashkour@gmail.com>
2019-12-17 14:32:52 +0100

    fix: improve styles

--- src/components/dashboard/components/Priorities/TicketsPriority.js
+++ src/components/dashboard/components/Priorities/TicketsPriority.js
@@ -29,9 +29,6 @@ const TicketsPriority = (props) => {
       padding: '1.5rem 0',
       fontSize: '2rem',
     },
-    card: {
-      marginRight: '0',
-    },
     p: {
       color: '#4c4c4c',
       fontSize: '16px',
@@ -41,7 +38,7 @@ const TicketsPriority = (props) => {
   };
 
   return (
-    <div className="card tickets-card" style={cardStyle.card}>
+    <div className="card customer-card" style={cardStyle.card}>
       <header className="card-header">
         <p className="card-header-title" style={cardStyle.p}>
           {t('settings.tickets_priority')}
