de56826c3f5ece12b934111ae74b7bf36a2d5e63
azizmashkour <azizmashkour@gmail.com>
2019-12-17 16:04:10 +0100

    fix: revert old changes

--- src/components/onboarding/Steps.js
+++ src/components/onboarding/Steps.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { Link } from 'react-router-dom';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
 import Button from '@material-ui/core/Button';
 import Header from '../layouts/Header';
+import AddAgent from './steps/AddAgent';
 import AssignAgent from './steps/AssignAgent';
+import AccountSummary from './steps/AccountSummary';
 import { options } from '../../configs/options';
 import SelectAgent from '../../assets/images/onboard/get-started/select-agent.png';
 import SingleService from '../../assets/images/onboard/group.png';
+
+const ColorlibConnector = withStyles({
+  alternativeLabel: {
+    top: 25,
+  },
+  active: {
+    '& $line': {
+      backgroundColor: '#00bd39',
+    },
+  },
+  completed: {
+    '& $line': {
+      backgroundColor: '#00bd39',
+    },
+  },
+  line: {
+    height: 3,
+    border: 0,
+    backgroundColor: '#c8d3d6',
+    borderRadius: 1,
+  },
+})(StepConnector);
 
 const useColorlibStepIconStyles = makeStyles({
   root: {
@@ -102,6 +130,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
 function getStepContent(
   step,
   handleChooseService,
@@ -113,11 +145,27 @@ function getStepContent(
   switch (step) {
     case 0:
       return (
+        <AddAgent
+          handleChooseService={handleChooseService}
+          checkedServices={checkedServices}
+        />
+      );
+    case 1:
+      return (
         <AssignAgent
           handleChooseService={handleChooseService}
           checkedServices={checkedServices}
           activeServices={activeServices}
           handleBack={handleBack}
+          containerWidth={containerWidth}
+        />
+      );
+    case 2:
+      return (
+        <AccountSummary
+          handleChooseService={handleChooseService}
+          checkedServices={checkedServices}
+          activeServices={activeServices}
           containerWidth={containerWidth}
         />
       );
@@ -129,6 +177,7 @@ function getStepContent(
 export default function Steps(props) {
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
+  const steps = getSteps();
   const classes = useStyles();
 
   const isStepSkipped = (step) => skipped.has(step);
@@ -146,6 +195,22 @@ export default function Steps(props) {
     }
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
     setSkipped(newSkipped);
+  };
+
+  const handleSkip = () => {
+    if (activeStep === 0) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 2);
+      setSkipped((prevSkipped) => {
+        const newSkipped = new Set(prevSkipped.values());
+        newSkipped.add(activeStep);
+        return newSkipped;
+      });
+    }
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+    props.selectServiceRef.current.click();
   };
 
   const {
@@ -178,9 +243,32 @@ export default function Steps(props) {
       <div className="steps-container">
         <div className="columns">
           <div className={`${(activeStep === 0 || (activeStep === 1 && (localService && localService.length === 1))) ? 'column is-three-fifths steps-column' : 'column is-full steps-column'}`}>
-            <h2 className="common-medium-title">{t('onboard.add_agent')}</h2>
-            <p>{t('onboard.add_agent_to_your_platform')}</p>
+            {activeStep === 0 && (
+              <h2 className="common-medium-title">{t('onboard.get_started')}</h2>
+            )}
+            {activeStep === 1 && (
+              <h2 className="common-medium-title">{t('onboard.assign_agent_to_channel')}</h2>
+            )}
+            {activeStep === 2 && (
+              <h2 className="common-medium-title">{t('onboard.account_setup_summary')}</h2>
+            )}
+            {activeStep === 0 && (
+              <p>{t('onboard.add_channels_to_your_platform')}</p>
+            )}
+            {activeStep === 1 && (
+              <p>{t('onboard.assign_agent_to_work')}</p>
+            )}
+            {activeStep === 2 && (
+              <p>{t('onboard.onboarding_process')}</p>
+            )}
             <div className={classes.root}>
+              <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />} className={classes.steppers}>
+                {steps.map((label, index) => (
+                  <Step key={label}>
+                    <StepLabel StepIconComponent={ColorlibStepIcon} onClick={handleStep(index)} />
+                  </Step>
+                ))}
+              </Stepper>
               <div>
                 <div className="content-selector">
                   <div className={classes.instructions}>
@@ -197,7 +285,17 @@ export default function Steps(props) {
                   </div>
                   <div className="next-back-container">
                     <Button onClick={handleSimulateChooseServices} ref={selectServiceRef} style={{ display: 'none' }}>Simulate</Button>
-                    <Link to="/dashboard" style={{ color: '#ffffff' }}>
+                    { activeStep === steps.length - 1 ? (
+                      <Link to="/dashboard" style={{ color: '#ffffff' }}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          className={classes.button}
+                        >
+                          {t('onboard.finish')}
+                        </Button>
+                      </Link>
+                    ) : (
                       <Button
                         variant="contained"
                         color="primary"
@@ -206,16 +304,17 @@ export default function Steps(props) {
                       >
                         {t('onboard.continue')}
                       </Button>
-                    </Link>
-                    <Link to="/dashboard" style={{ color: '#ffffff' }}>
+                    )}
+                    {activeStep !== steps.length - 1 && (
                       <Button
                         variant="outlined"
                         color="primary"
+                        onClick={activeStep === 1 ? handleNext : handleSkip}
                         className={classes.buttonOutlined}
                       >
                         {t('onboard.skip')}
                       </Button>
-                    </Link>
+                    )}
                   </div>
                 </div>
               </div>
