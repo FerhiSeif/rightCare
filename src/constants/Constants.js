ec2cfcd882524abff7f04dbc75e58cc332da42ff
jordy ndri <jordy@right-com.com>
2019-12-30 12:13:53 +0000

    Fix: refact file Constant.js (API Domaine)

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -2,24 +2,38 @@
 import uuid1 from 'uuid/v1';
 
 /* eslint-disable no-nested-ternary */
-export const APP_ENV = 'LOCAL'; // DEV, PROD, LOCAL
+export const APP_ENV = 'DEV'; // DEV, PROD, LOCAL
 export const DEBUG = true;
 export const TIMEOUT_HTTP_REQUEST = 3000;
 export const TIMEOUT_SOCKET_RESPONSE = 5000;
 
 export const PROTOCOL = 'http';
-export const RC_API_BASE_DOMAIN_PROD = 'betarsapi.rightcom.co';
-export const RC_API_BASE_DOMAIN_DEV = '10.10.14.116:715';
+// 10.10.14.116:715 , betarcapi.right-com.com
+export const RC_API_BASE_DOMAIN_PROD = 'betarcapi.right-com.com';
+export const RC_API_BASE_DOMAIN_DEV = 'betarcapi.right-com.com';
 export const RC_API_BASE_DOMAIN_LOCAL = '10.10.14.116:715';
-export const RC_API_SERVICE_PATH_PROD = '/api/v1';
-export const RC_API_SERVICE_PATH_DEV = '/api/v1';
-export const RC_API_SERVICE_PATH_LOCAL = '/api/v1';
-export const RC_SOCKET_BASE_DOMAIN_PROD = 'betarsapi-socket.rightcom.co';
-export const RC_SOCKET_BASE_DOMAIN_DEV = '10.10.14.116:714';
+export const RC_API_SERVICE_PATH = '/api/v1';
+// 10.10.14.116:714 , betarcapi-socket.right-com.com
+export const RC_SOCKET_BASE_DOMAIN_PROD = 'betarcapi-socket.right-com.com';
+export const RC_SOCKET_BASE_DOMAIN_DEV = 'betarcapi-socket.right-com.com';
 export const RC_SOCKET_BASE_DOMAIN_LOCAL = '10.10.14.116:714';
-export const RC_SOCKET_SERVICE_PATH_PROD = '/myrc';
-export const RC_SOCKET_SERVICE_PATH_DEV = '/myrc';
-export const RC_SOCKET_SERVICE_PATH_LOCAL = '/myrc';
+export const RC_SOCKET_SERVICE_PATH = '/myrc';
+
+export const API = {
+  BASE_URL: APP_ENV === 'DEV' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_DEV}${RC_API_SERVICE_PATH}`
+    : APP_ENV === 'PROD' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_PROD}${RC_API_SERVICE_PATH}`
+      : APP_ENV === 'LOCAL' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_LOCAL}${RC_API_SERVICE_PATH}`
+        : '',
+  // SIGNIN: '/signin',
+  // CHECK_SESSION: '/decoder',
+};
+
+export const SOCKET = {
+  BASE_URL: APP_ENV === 'DEV' ? `${RC_SOCKET_BASE_DOMAIN_DEV}${RC_SOCKET_SERVICE_PATH}`
+    : APP_ENV === 'PROD' ? `${RC_SOCKET_BASE_DOMAIN_PROD}${RC_SOCKET_SERVICE_PATH}`
+      : APP_ENV === 'LOCAL' ? `${RC_SOCKET_BASE_DOMAIN_LOCAL}${RC_SOCKET_SERVICE_PATH}`
+        : '',
+};
 
 export const NAVIGATION = {
   HOME: '/',
@@ -48,22 +62,6 @@ export const CONSTANTS_LANG = {
       name: 'french',
     },
   ],
-};
-
-export const API = {
-  BASE_URL: APP_ENV === 'DEV' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_DEV}${RC_API_SERVICE_PATH_DEV}`
-    : APP_ENV === 'PROD' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_PROD}${RC_API_SERVICE_PATH_PROD}`
-      : APP_ENV === 'LOCAL' ? `${PROTOCOL}://${RC_API_BASE_DOMAIN_LOCAL}${RC_API_SERVICE_PATH_LOCAL}`
-        : '',
-  SIGNIN: '/signin',
-  CHECK_SESSION: '/decoder',
-};
-
-export const SOCKET = {
-  BASE_URL: APP_ENV === 'DEV' ? `${RC_SOCKET_BASE_DOMAIN_DEV}${RC_SOCKET_SERVICE_PATH_DEV}`
-    : APP_ENV === 'PROD' ? `${RC_SOCKET_BASE_DOMAIN_PROD}${RC_SOCKET_SERVICE_PATH_PROD}`
-      : APP_ENV === 'LOCAL' ? `${RC_SOCKET_BASE_DOMAIN_LOCAL}${RC_SOCKET_SERVICE_PATH_LOCAL}`
-        : '',
 };
 
 export const SIO_TICKET_SETTINGS = `ticket_setting_${uuid1()}`;
